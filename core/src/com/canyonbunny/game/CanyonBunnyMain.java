95b73134d122d8a261264c6605bbc6e831c62272
phuongthanh9502 <navynguyen9502@gmail.com>
2021-09-17 00:04:04 +0700

    Thiet lap game canyon bunny

--- core/src/com/canyonbunny/game/CanyonBunnyMain.java
+++ core/src/com/canyonbunny/game/CanyonBunnyMain.java
@@ -1,31 +1,38 @@
 package com.canyonbunny.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class CanyonBunnyMain extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+	 private static final String TAG = CanyonBunnyMain.class.getName();
+		private WorldController worldController;
+		private WorldRenderer worldRenderer;
+		  @Override 
+		    public void create() {
+			  worldController = new WorldController();
+			  worldRenderer = new WorldRenderer(worldController);
+		  }
+		  @Override 
+		  public void render() {
+			  worldController.update(Gdx.graphics.getDeltaTime());
+				// Sets the clear screen color to: Cornflower Blue
+				Gdx.gl.glClearColor(0x64/255.0f, 0x95/255.0f, 0xed/255.0f, 0xff/255.0f);
+				// Clears the screen
+				Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+			  
+		  }
+		  @Override 
+		  public void resize(int width, int height) {
+			  worldRenderer.resize(width, height);
+		  }
+		  @Override 
+		  public void pause() {}
+		  @Override 
+		  public void resume() {}
+		  @Override 
+		  public void dispose() {}
 }
